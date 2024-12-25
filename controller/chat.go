767879ed458ff5ec54e7b304ab3e5a158a325682
dean <1006393151@qq.com>
2024-12-26 00:30:22 +0800

    fix: log

--- controller/chat.go
+++ controller/chat.go
@@ -522,6 +522,7 @@ func makeRequest(client cycletls.CycleTLS, jsonData []byte, cookie string, isStr
 		Body:    string(jsonData),
 		Method:  "POST",
 		Headers: map[string]string{
+			"User-Agent":   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
 			"Content-Type": "application/json",
 			"Accept":       accept,
 			"Origin":       baseURL,
