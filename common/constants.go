145133f8208ab16ee1f18d6a7b61faac41032f11
dean <1006393151@qq.com>
2025-01-23 10:30:23 +0800

    docs

--- common/constants.go
+++ common/constants.go
@@ -3,7 +3,7 @@ package common
 import "time"
 
 var StartTime = time.Now().Unix() // unit: second
-var Version = "v1.8.4"            // this hard coding will be replaced automatically when building, no need to manually change
+var Version = "v1.8.5"            // this hard coding will be replaced automatically when building, no need to manually change
 
 var DefaultOpenaiModelList = []string{
 	"gpt-4o",
@@ -14,6 +14,7 @@ var DefaultOpenaiModelList = []string{
 	"gemini-1.5-pro",
 	"gemini-1.5-flash",
 	"deep-seek-v3",
+	"deep-seek-r1",
 
 	"flux",
 	"flux-speed",
@@ -31,6 +32,8 @@ var TextModelList = []string{
 	"claude-3-5-haiku",
 	"gemini-1.5-pro",
 	"gemini-1.5-flash",
+	"deep-seek-v3",
+	"deep-seek-r1",
 }
 
 var MixtureModelList = []string{
