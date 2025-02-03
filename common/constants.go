c4d5319c41c8a323eed2a0b99dfffa04368a48ad
dean <1006393151@qq.com>
2025-02-03 23:21:53 +0800

    docs

--- common/constants.go
+++ common/constants.go
@@ -3,7 +3,7 @@ package common
 import "time"
 
 var StartTime = time.Now().Unix() // unit: second
-var Version = "v1.8.7"            // this hard coding will be replaced automatically when building, no need to manually change
+var Version = "v1.8.8"            // this hard coding will be replaced automatically when building, no need to manually change
 
 var DefaultOpenaiModelList = []string{
 	"gpt-4o",
